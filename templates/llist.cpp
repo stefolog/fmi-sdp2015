9aeb09346d3013747a9645ce48e2a92daa094d4b
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2016-01-07 08:47:05 +0200

    Update graph template

--- templates/llist.cpp
+++ templates/llist.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 template <class T>
 struct elem
-{T inf;
- elem *link;
+{
+  T inf;
+  elem *link;
 };
 
 template <class T>
 class LList
-{public:
+{
+public:
  LList();
   ~LList();
   LList(LList const&);
@@ -24,12 +26,13 @@ class LList
   int DeleteAfter(elem<T> *, T &);
   int DeleteBefore(elem<T> *, T &);
   void DeleteElem(elem<T> *, T &);
+  bool empty();
   int len();
   void concat(LList const&);
   void reverse();
 
   T operator[](int idx);
- private:
+private:
   elem<T> *Start, *End, *Current;
   void DeleteList();
   void CopyList(LList<T> const &);
@@ -124,21 +127,23 @@ elem<T>* LList<T>::Iter()
 
 template <class T>
 void LList<T>::ToEnd(T const & x)
-{Current = End;
- End = new elem<T>;
- End->inf = x;
- End->link = NULL;
- if(Current) Current->link = End;
- else Start = End;
+{
+  Current = End;
+  End = new elem<T>;
+  End->inf = x;
+  End->link = NULL;
+  if(Current) Current->link = End;
+  else Start = End;
 }
 
 template <class T>
 void LList<T>::InsertAfter(elem<T> *p, T const & x)
-{elem<T> *q = new elem<T>;
- q->inf = x;
- q->link = p->link;
- if(p==End) End = q;
- p->link = q;
+{
+  elem<T> *q = new elem<T>;
+  q->inf = x;
+  q->link = p->link;
+  if(p==End) End = q;
+  p->link = q;
 }
 
 template <class T>
@@ -153,30 +158,35 @@ void LList<T>::InsertBefore(elem<T> * p, T const& x)
 
 template <class T>
 int LList<T>::DeleteAfter(elem<T> *p, T &x)
-{if(p->link)
-{elem<T> *q = p->link;
- x = q->inf;
- p->link = q->link;
- if(q==End) End = p;
- delete q;
- return 1;
- }
- else return 0;
+{
+  if(p->link)
+  {
+    elem<T> *q = p->link;
+    x = q->inf;
+    p->link = q->link;
+    if(q==End) End = p;
+    delete q;
+    return 1;
+  }
+  else return 0;
 }
 
 template <class T>
 void LList<T>::DeleteElem(elem<T> *p, T &x)
-{if(p==Start)
- {x = p->inf;
-  if(Start == End){Start = End = NULL;}
-  else Start = Start->link;
-  delete p;
- }
- else
- {elem<T> *q = Start;
-  while(q->link!=p)q = q->link;
-  DeleteAfter(q, x);
- }
+{
+  if(p==Start)
+  {
+    x = p->inf;
+    if(Start == End){Start = End = NULL;}
+    else Start = Start->link;
+    delete p;
+  }
+  else
+  {
+    elem<T> *q = Start;
+    while(q->link!=p)q = q->link;
+    DeleteAfter(q, x);
+  }
 }
 
 template <class T>
@@ -189,6 +199,12 @@ int LList<T>::DeleteBefore(elem<T> *p, T &x)
     DeleteElem(q, x);
     return 1;
   } else return 0;
+}
+
+template <class T>
+bool LList<T>::empty()
+{
+  return End == NULL;
 }
 
 template <class T>
